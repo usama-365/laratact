0b58efd4a2cf882b66fb4c46a7dbed5f6c5bcb9a
me <usama-365@protonmail.com>
2022-06-30 17:59:51 +0500

    Created the contact model that maps with contacts table

---
+++ app/Models/Contact.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    protected $table = 'contacts';
+
+    // Inverse relationship method to match with company
+    public function company()
+    {
+        // Eloquent will try to find a company which 'id' matches with the 'company_id' value
+        // We can also provide a different foreign key attribute name here
+        return $this->belongsTo(Company::class);
+    }
+}
