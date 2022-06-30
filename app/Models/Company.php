aaf7a9929cf6905c89343a72ddee212517297c79
me <usama-365@protonmail.com>
2022-06-30 15:41:06 +0500

    Made the company model mass assignable

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -9,4 +9,6 @@ class Company extends Model
 {
     // Can override the table that is mapped with this model.
     protected $table = "companies";
+    // Specify the columns that are fillable
+    protected $fillable = ['name', 'address', 'email', 'website'];
 }
