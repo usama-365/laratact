bfe47ccadbc204156e7a3bb2f0fd6a1e907fe9c6
me <usama-365@protonmail.com>
2022-06-30 16:18:02 +0500

    Added the migration to create contacts table

---
+++ database/migrations/2022_06_30_104328_create_contacts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id()->unsigned();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone')->nullable();
+            $table->string('email');
+            $table->string('address');
+            $table->timestamps();
+            $table->bigInteger('company_id')->unsigned();
+            // Adding the foreign key relation
+            $table->foreign('company_id')->references('id')->on('companies')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
