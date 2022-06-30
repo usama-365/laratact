b78067cfd052f74fe1c71c65754d5239cc3d4e17
me <usama-365@protonmail.com>
2022-06-30 15:24:21 +0500

    Created the Company model that maps with companies table

---
+++ app/Models/Company.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    // Can override the table that is mapped with this model.
+    protected $table = "companies";
+}
